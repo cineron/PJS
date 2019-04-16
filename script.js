6699a7a2aedfc92f8e646f09ff1cd641f0e6afdf
cineron <cineron+github@gmail.com>
2019-04-16 12:49:14 -0500

    Adds click handler

--- script.js
+++ script.js
@@ -1,74 +1,78 @@
-// V5 Requirements
-// - .toggleAll: if everything is true, make everyting false.
-// - .toggleAll: Otherwise, make everything true.
+// V7 Requirements
+// - There should be a "Display todos" button and .toggleAll button
+// - clicking "display todos" should run .displayTodos
+// - clicking "toggle all" should run .toggleAll
 
 var todoList = {
 	todos: [],
 	displayTodos: function() {
 		if (this.todos.length === 0) {
-		console.log("Your todo list is empty!")
+			console.log("Your todo list is empty!");
 		} else {
-		console.log("My todos:");
-		for (let i = 0; i < this.todos.length; i++){
-		// console.log(this.todos[i].todoText);
-		//check if .completed
-		if (this.todos[i].completed) {
-			console.log(`(x) ${this.todos[i].todoText}`)
-		} else {
-			console.log(`( ) ${this.todos[i].todoText}`)
-		}
-		};
+			console.log("My todos:");
+			for (let i = 0; i < this.todos.length; i++) {
+				// console.log(this.todos[i].todoText);
+				//check if .completed
+				if (this.todos[i].completed) {
+					console.log(`(x) ${this.todos[i].todoText}`);
+				} else {
+					console.log(`( ) ${this.todos[i].todoText}`);
+				}
+			}
 		}
 	},
-	addTodo: function(todoText){
-	  this.todos.push({
-		todoText: todoText,
-		completed: false
-	  });
-	  this.displayTodos();
+addTodo: function(todoText) {
+		this.todos.push({
+			todoText: todoText,
+			completed: false
+		});
+		this.displayTodos();
 	},
 	changeTodo: function(position, todoText) {
-	  this.todos[position].todoText = todoText;
-	  this.displayTodos();
+		this.todos[position].todoText = todoText;
+		this.displayTodos();
 	},
 	deleteTodo: function(position) {
-	  this.todos.splice(position, 1);
-	  this.displayTodos();
+		this.todos.splice(position, 1);
+		this.displayTodos();
 	},
-	toggleCompleted: function(position){
-	  // this works
-	  let completedTodo = this.todos[position];
-	  completedTodo.completed = !completedTodo.completed
-	  
-	  // this does not work! WHY??? -- because the boolean by itself doesn't apply to a specific object.
-	  // var done = this.todos[position].completed;
-	  // done = !done;
-	  this.displayTodos();
-	},
-	toggleAll: function(){
-	  const totalTodos = this.todos.length;
-	  let completedTodos = 0;
-	  for (let i = 0; i < totalTodos; i++){
-		if (this.todos[i].completed === true){
-		  completedTodos++;
-		}
-	  }
-	  //if everything's true, make everything false
-	  if (completedTodos === totalTodos){
-		//make everything false
-		for (let i =0; i < totalTodos; i++){
-		  this.todos[i].completed = false;
-		}
-	   //make everything false
-	  } else {
-		for (let i = 0; i < totalTodos; i++) {
-		  this.todos[i].completed = true;
-		}
-	  }
-	  this.displayTodos();
-	}
-	};
-	
+	toggleCompleted: function(position) {
+		// this works
+		let completedTodo = this.todos[position];
+		completedTodo.completed = !completedTodo.completed;
 
-	
-	
+		// this does not work! WHY??? -- because the boolean by itself doesn't apply to a specific object.
+		// var done = this.todos[position].completed;
+		// done = !done;
+		this.displayTodos();
+	},
+	toggleAll: function() {
+		const totalTodos = this.todos.length;
+		let completedTodos = 0;
+		for (let i = 0; i < totalTodos; i++) {
+			if (this.todos[i].completed === true) {
+				completedTodos++;
+			}
+		}
+		//if everything's true, make everything false
+		if (completedTodos === totalTodos) {
+			//make everything false
+			for (let i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = false;
+			}
+			//make everything false
+		} else {
+			for (let i = 0; i < totalTodos; i++) {
+				this.todos[i].completed = true;
+			}
+		}
+		this.displayTodos();
+	}
+};
+
+// 1. Accesss the "display todos" button
+var displayTodosButton = document.getElementById("DisplayTodos");
+// 2. run .displayTodos when the button is clicked
+displayTodosButton.addEventListener("click", function(){
+	todoList.displayTodos();
+});
